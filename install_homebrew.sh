7678bf06eed3fcc61cf9be1c13d7325fb34fbb42
Henry Ou <henryouly@gmail.com>
2014-08-12 13:41:17 -0700

    execute

--- install_homebrew.sh
+++ install_homebrew.sh
@@ -1,2 +1,6 @@
 HOMEBREW_DIR=${HOME}/homebrew
 mkdir ${HOMEBREW_DIR} && curl -L https://github.com/Homebrew/homebrew/tarball/master | tar xz --strip 1 -C ${HOMEBREW_DIR}
+PATH=${PATH}:${HOMEBREW_DIR}
+brew update
+brew install coreutils 
+brew install android-sdk maven
